78d7fba3cf753dc7ec308d0d03a3ede7072a0352
vladislav-plotnikoff <42ebox@gmail.com>
2015-10-13 13:30:40 +0600

    Еще поправка к ресайзу.
    Остался небольшой косяк. Немного по разному ведут себя стороны при
    уменьшении размеров к минимуму.

--- Connect/Connect/Form1.cs
+++ Connect/Connect/Form1.cs
@@ -90,43 +90,25 @@ namespace Connect
 		const int WMSZ_RIGHT = 2;
 		const int WMSZ_TOP = 3;
 		const int WMSZ_BOTTOM = 6;
-		public struct RECT {
-			public int Left;
-			public int Top;
-			public int Right;
-			public int Bottom;
-		}
+
 		protected override void WndProc(ref Message m) {
 			if(m.Msg == WM_SIZING) {
-				RECT rc = (RECT)Marshal.PtrToStructure(m.LParam, typeof(RECT));
+				Rectangle rc = (Rectangle)Marshal.PtrToStructure(m.LParam, typeof(Rectangle));
 				int res = m.WParam.ToInt32();
-				if(res == WMSZ_LEFT || res == WMSZ_RIGHT) {
-					//Left or right resize -> adjust height (bottom)
-					rc.Bottom = rc.Top + Width;;
-				}
-				else if(res == WMSZ_TOP || res == WMSZ_BOTTOM) {
-					//Up or down resize -> adjust width (right)
-					rc.Right = rc.Left + Height;
-				}
-				else if(res == WMSZ_RIGHT + WMSZ_BOTTOM) {
-					//Lower-right corner resize -> adjust height (could have been width)
-					rc.Bottom = rc.Top + Width;
-				}
-				else if(res == WMSZ_LEFT + WMSZ_TOP) {
+				if(res == WMSZ_LEFT + WMSZ_TOP) {
 					//Upper-left corner -> adjust width (could have been height)
-					rc.Left = rc.Right - Height;
+					rc.X = rc.Width - Height;
 				}
-				else if(res == WMSZ_LEFT + WMSZ_BOTTOM) {
-					//
-					rc.Bottom = rc.Top + Width;
+				else if(res == WMSZ_TOP || res == WMSZ_BOTTOM || res == WMSZ_RIGHT + WMSZ_TOP || res == WMSZ_RIGHT + WMSZ_TOP) {
+					//Up or down resize -> adjust width (right)
+					rc.Width = rc.Left + Height;
 				}
-				else if(res == WMSZ_RIGHT + WMSZ_TOP) {
-					//
-					rc.Right = rc.Left + Height;
+				else if(res == WMSZ_LEFT || res == WMSZ_RIGHT || res == WMSZ_RIGHT + WMSZ_BOTTOM || res == WMSZ_LEFT + WMSZ_BOTTOM) {
+					//Lower-right corner resize -> adjust height (could have been width)
+					rc.Height = rc.Top + Width;
 				}
 				Marshal.StructureToPtr(rc, m.LParam, true);
 			}
-
 			base.WndProc(ref m);
 		}
 	}
